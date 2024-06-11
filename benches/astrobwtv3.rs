cb4288cd2e02cd2185b640a9b8c54ed184758fcd
Slixe <slixeprivate@gmail.com>
2024-06-11 22:37:20 +0200

    use fixed input for bench due to complexity of algorithm

--- benches/astrobwtv3.rs
+++ benches/astrobwtv3.rs
@@ -6,13 +6,13 @@ use criterion::Criterion;
 // Private crates.
 use spectrex::astrobwtv3;
 
-fn astrobwtv3_bench() {
-    let input: [u8; 32] = rand::random();
-    astrobwtv3::astrobwtv3_hash(&input);
+fn astrobwtv3_bench(input: &[u8; 32]) {
+    astrobwtv3::astrobwtv3_hash(input);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("astrobwtv3", |b| b.iter(|| astrobwtv3_bench()));
+    let input: [u8; 32] = [0; 32];
+    c.bench_function("astrobwtv3", |b| b.iter(|| astrobwtv3_bench(&input)));
 }
 
 criterion_group!(
